e7846aad362e1e6f29b2bcf0a5ce1a6448143b37
M. Robles <roble146@umn.edu>
2024-04-28 13:22:20 -0500

    Applied polynomial curve fit to height and range data over time for 100 trajectories.

--- PaperPlane.m
+++ PaperPlane.m
@@ -5,6 +5,8 @@
 %   (w/multiple ~100 trajectories) with a single curve fit. 
 %   Recommended to use a single line color
 
+% Why isn't t the same length??
+
 clear; clc; close all;
 
 %	a) Equilibrium Glide at Maximum Lift/Drag Ratio
@@ -15,6 +17,7 @@ clear; clc; close all;
 	xo		=	[V;Gam;H;R];
 	[ta,xa]	=	ode23('EqMotion',tspan,xo);
 	
+%% Part 2
 %	b1) Oscillating Glide due to Zero Initial Flight Path Angle
 %           and lower Initial Velocity -- DO WE LEAVE GAM = 0??
 	xo		=	[2;Gam;H;R];
@@ -55,32 +58,50 @@ clear; clc; close all;
             plot(xc1(:,4),xc1(:,3),"k",xd1(:,4),xd1(:,3),"g",xb1(:,4),xb1(:,3),"r");
             title(["Height vs. Range", "Varying Initial Velocity"], "FontName", "Times New Roman");
             xlabel('Range (m)', "FontName", "Times New Roman"), ylabel('Height (m)', "FontName", "Times New Roman"), grid
-            legend("Nominal", "Higher", "Lower", "FontName", "Times New Roman");
+            legend("Nominal (3.55 m/s)", "Higher (7.5 m/s)", "Lower (2 m/s)", "FontName", "Times New Roman");
         else
             plot(xc2(:,4),xc2(:,3),"k",xd2(:,4),xd2(:,3),"g", xb2(:,4),xb2(:,3),"r");
             title(["Height vs. Range", "Varying Initial Flight Path Angle"], "FontName", "Times New Roman");
             xlabel('Range (m)', "FontName", "Times New Roman"), ylabel('Height (m)', "FontName", "Times New Roman"), grid
-            legend("Nominal", "Higher", "Lower", "FontName", "Times New Roman");
+            legend("Nominal (-0.18 rad)", "Higher (0.4 rad)", "Lower (-0.5 rad)", "FontName", "Times New Roman");
         end
     end
 
+%% Parts 3 and 4
 figure; hold on;
 title(["Height vs. Range", "Random Parameters"], "FontName", "Times New Roman");
 xlabel('Range (m)', "FontName", "Times New Roman"), ylabel('Height (m)', "FontName", "Times New Roman"), grid
 % Change tspan?
-% Plotting H vs. R? Only Changing V and gam randomly?
+% Plotting H vs. R? Only Changing V and gam randomly or all 4 vars?
 Vrange = [2, 7.5];
 Gamrange = [-0.5, 0.4];
+H_array = zeros(100, 1);
+R_array = zeros(100, 1);
+h = 6/99;
+tspan = 0:h:6;
 for i = 1:100
     Vrand = Vrange(1) + (Vrange(2)-Vrange(1))*rand(1);
     Gamrand = Gamrange(1) + (Gamrange(2)-Gamrange(1))*rand(1);
-    xo		=	[Vrand;Gamrand;H;R];
+    Hrand = rand(1);
+    Rrand = rand(1);
+    xo		=	[Vrand;Gamrand;Hrand;Rrand];
 	[t,x]	=	ode23('EqMotion',tspan,xo);   
-    plot(x(:,4),x(:,3), "Color", [190, 190, 190]/255);
+    plot(x(:,4),x(:,3), "Color", [190, 190, 190]/255); 
     hold on;
+    H_array(i) = xo(3);
+    R_array(i) = xo(4);
 end
 
+p_H = polyfit(t, H_array, 4); 
+H_fit = polyval(p_H, t); 
+p_R = polyfit(t, R_array, 4);
+R_fit = polyval(p_R, t);
 
+%% Part 5
+
+
+
+%% Extra Stuff
 % 	figure
 % 	subplot(2,2,1)
 % 	plot(ta,xa(:,1),tb,xb(:,1),tc,xc(:,1),td,xd(:,1))
