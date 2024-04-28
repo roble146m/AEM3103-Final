3839ba6ae4365a866878c2ca01483f4d3f9337e7
M. Robles <roble146@umn.edu>
2024-04-28 13:40:11 -0500

    Created central finite difference derivative function.

---
+++ central_der.m
@@ -0,0 +1,10 @@
+function [fp_num] = central_der(x, y)
+
+    drl = length(x); 
+    fp_num = nan*zeros(1, drl);
+
+    for i = 2:drl-1
+        fp_num(i) = (y(i+1) - y(i-1))/(x(i+1) - x(i-1));
+    end
+
+end
