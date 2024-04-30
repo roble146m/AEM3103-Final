65e08d2ebf327eb6c26e6d3b3f964aa87c302147
M. Robles <roble146@umn.edu>
2024-04-29 21:52:47 -0500

    Change to concatenation of 100 trajectories; created time, range, and height arrays, applied 10th polynomial curve fit to average trajectory and plotted on Fig. 2.

--- PaperPlane.m
+++ PaperPlane.m
@@ -1,11 +1,6 @@
 %	Example 1.3-1 Paper Airplane Flight Path
 %	Copyright 2005 by Robert Stengel
 %	August 23, 2005
-% For part 3: Don't need params. vs. time graphs, want height vs. range 
-%   (w/multiple ~100 trajectories) with a single curve fit. 
-%   Recommended to use a single line color
-
-% Why isn't t the same length??
 
 clear; clc; close all;
 
@@ -19,7 +14,7 @@ clear; clc; close all;
 	
 %% Part 2
 %	b1) Oscillating Glide due to Zero Initial Flight Path Angle
-%           and lower Initial Velocity -- DO WE LEAVE GAM = 0??
+%           and lower Initial Velocity 
 	xo		=	[2;Gam;H;R];
 	[tb1,xb1]	=	ode23('EqMotion',tspan,xo);
 
@@ -43,11 +38,6 @@ clear; clc; close all;
 	xo		=	[V;0.4;H;R];
 	[td2,xd2]	=	ode23('EqMotion',tspan,xo);
 
-% 	figure
-% 	plot(xa(:,4),xa(:,3),xb1(:,4),xb1(:,3),xc1(:,4),xc1(:,3),xd1(:,4),xd1(:,3))
-% 	xlabel('Range, m'), ylabel('Height, m'), grid
-%   legend("a", "b1", "c1", "d1");
-
     figure; hold on;
     % TODO: Change labels font size and axes number font/size
     ords = [1, 2];
@@ -56,79 +46,67 @@ clear; clc; close all;
         subplot(2, 1, i);
         if i == 1
             plot(xc1(:,4),xc1(:,3),"k",xd1(:,4),xd1(:,3),"g",xb1(:,4),xb1(:,3),"r");
-            title(["Height vs. Range", "Varying Initial Velocity"], "FontName", "Times New Roman");
-            xlabel('Range (m)', "FontName", "Times New Roman"), ylabel('Height (m)', "FontName", "Times New Roman"), grid
+            title(["Height vs. Range", "Varying Initial Velocity"], "FontName", "Times New Roman", "FontSize", 18);
+            xlabel('Range (m)', "FontName", "Times New Roman", "FontSize", 14), ylabel('Height (m)', "FontName", "Times New Roman", "FontSize", 14); grid;
             legend("Nominal (3.55 m/s)", "Higher (7.5 m/s)", "Lower (2 m/s)", "FontName", "Times New Roman");
         else
             plot(xc2(:,4),xc2(:,3),"k",xd2(:,4),xd2(:,3),"g", xb2(:,4),xb2(:,3),"r");
-            title(["Height vs. Range", "Varying Initial Flight Path Angle"], "FontName", "Times New Roman");
-            xlabel('Range (m)', "FontName", "Times New Roman"), ylabel('Height (m)', "FontName", "Times New Roman"), grid
+            title(["Height vs. Range", "Varying Initial Flight Path Angle"], "FontName", "Times New Roman", "FontSize", 18);
+            xlabel('Range (m)', "FontName", "Times New Roman", "FontSize", 14), ylabel('Height (m)', "FontName", "Times New Roman", "FontSize", 14); grid;
             legend("Nominal (-0.18 rad)", "Higher (0.4 rad)", "Lower (-0.5 rad)", "FontName", "Times New Roman");
         end
     end
 
 %% Parts 3 and 4
 figure; hold on;
-title(["Height vs. Range", "Random Parameters"], "FontName", "Times New Roman");
-xlabel('Range (m)', "FontName", "Times New Roman"), ylabel('Height (m)', "FontName", "Times New Roman"), grid
-% Change tspan?
-% Plotting H vs. R? Only Changing V and gam randomly or all 4 vars?
+title(["Height vs. Range", "Random Velocity and Flight Path Angle Parameters"], "FontName", "Times New Roman", "FontSize", 18);
+xlabel('Range (m)', "FontName", "Times New Roman", "FontSize", 14), ylabel('Height (m)', "FontName", "Times New Roman", "FontSize", 14); grid;
+% Only changing V and Gam randomly or all 4 vars?
+% Change oppacity
 Vrange = [2, 7.5];
 Gamrange = [-0.5, 0.4];
-H_array = zeros(100, 1);
-R_array = zeros(100, 1);
+H_array = zeros(100);
+R_array = zeros(100);
+t_array = zeros(100);
 h = 6/99;
 tspan = 0:h:6;
 for i = 1:100
     Vrand = Vrange(1) + (Vrange(2)-Vrange(1))*rand(1);
     Gamrand = Gamrange(1) + (Gamrange(2)-Gamrange(1))*rand(1);
-    Hrand = rand(1);
-    Rrand = rand(1);
-    xo		=	[Vrand;Gamrand;Hrand;Rrand];
+    xo		=	[Vrand;Gamrand;H;R];
 	[t,x]	=	ode23('EqMotion',tspan,xo);   
-    plot(x(:,4),x(:,3), "Color", [190, 190, 190]/255); 
+    plot(x(:,4),x(:,3), "Color", [125, 125, 125]/255); % 107?
     hold on;
-    H_array(i) = xo(3);
-    R_array(i) = xo(4);
+    H_array(:, i) = x(:, 3);
+    R_array(:, i) = x(:, 4);
+    t_array(:, i) = t(:, :);
 end
 
-% Poly order? 
-poly_H = polyfit(t, H_array, 4); 
+H_array = H_array(:);
+R_array = R_array(:);
+t_array = t_array(:);
+
+% Polyn. order? 
+poly_H = polyfit(t_array, H_array, 10); 
 H_fit = polyval(poly_H, t); 
-poly_R = polyfit(t, R_array, 4);
+poly_R = polyfit(t_array, R_array, 10);
 R_fit = polyval(poly_R, t);
+plot(R_fit, H_fit, "r", "LineWidth", 2); % "N-th order poly fit of..."
 
 %% Part 5
-
 H_der = central_der(t, H_fit);
 R_der = central_der(t, R_fit);
 figure; hold on;
 for i = 1:2
     subplot(2, 1, i);
     if i == 1
-        plot(t, H_der, 'b');
-        title("Time Derivate of Height", "FontName", "Times New Roman")
-        xlabel("Time (sec)", "FontName", "Times New Roman"); ylabel("Height Derive (m/sec)", "FontName", "Times New Roman");
+        plot(t, H_der, 'b', "LineWidth", 2);
+        title("Time Derivate of Height", "FontName", "Times New Roman", "FontSize", 18)
+        xlabel("Time (sec)", "FontName", "Times New Roman", "FontSize", 14); ylabel("Height Derive (m/sec)", "FontName", "Times New Roman", "FontSize", 14); grid;
     end
     if i == 2
-        plot(t, R_der, "r");
-        title("Time Derivate of Range", "FontName", "Times New Roman")
-        xlabel("Time (sec)", "FontName", "Times New Roman"); ylabel("Range Derive (m/sec)", "FontName", "Times New Roman");
+        plot(t, R_der, "r", "LineWidth", 2);
+        title("Time Derivate of Range", "FontName", "Times New Roman", "FontSize", 18)
+        xlabel("Time (sec)", "FontName", "Times New Roman", "FontSize", 14); ylabel("Range Derive (m/sec)", "FontName", "Times New Roman", "FontSize", 14); grid;
     end
 end
-
-%% Extra Stuff
-% 	figure
-% 	subplot(2,2,1)
-% 	plot(ta,xa(:,1),tb,xb(:,1),tc,xc(:,1),td,xd(:,1))
-% 	xlabel('Time, s'), ylabel('Velocity, m/s'), grid
-% 	subplot(2,2,2)
-% 	plot(ta,xa(:,2),tb,xb(:,2),tc,xc(:,2),td,xd(:,2))
-% 	xlabel('Time, s'), ylabel('Flight Path Angle, rad'), grid
-% 	subplot(2,2,3)
-% 	plot(ta,xa(:,3),tb,xb(:,3),tc,xc(:,3),td,xd(:,3))
-% 	xlabel('Time, s'), ylabel('Altitude, m'), grid
-% 	subplot(2,2,4)
-% 	plot(ta,xa(:,4),tb,xb(:,4),tc,xc(:,4),td,xd(:,4))
-% 	xlabel('Time, s'), ylabel('Range, m'), grid
-
